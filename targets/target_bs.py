6d2c4f8253bd90e0935ae8df3d4cd95b64e7b28d
lgx <lgx@live.com>
2012-09-14 11:39:26 +0800

    Use the newest version of BS & LXML

--- targets/target_bs.py
+++ targets/target_bs.py
@@ -3,18 +3,19 @@
 
 """ Test Target: BeautifulSoup """
 
-from BeautifulSoup import BeautifulSoup
+from bs4 import __version__ as VERSION
+from bs4 import BeautifulSoup
 
 from target import CTarget
 
 
 class CTargetBS(CTarget):
     name = 'BeautifulSoup'
-    version = '3.2.0'
+    version = VERSION
 
     def _test(self, sample):
         """ Actual Test Enrty """
         html = sample['content']
-        elapsed = self.timeit(lambda: BeautifulSoup(html, fromEncoding="UTF-8"))
+        elapsed = self.timeit(lambda: BeautifulSoup(html, from_encoding="UTF-8"))
         return {'time': elapsed}
 
--- targets/target_lxml.py
+++ targets/target_lxml.py
@@ -4,16 +4,18 @@
 """ Test Target: lxml """
 
 import lxml.html
+from lxml import etree
 
 from target import CTarget
 
 
 class CTargetLXML(CTarget):
     name = 'lxml'
-    version = '2.3.4'
+    version = '2.3.5'
 
     def _test(self, sample):
         """ Actual Test Enrty """
+        etree.clear_error_log()
         html = sample['content']
         elapsed = self.timeit(lambda: lxml.html.fromstring(html))
         return {'time': elapsed}
